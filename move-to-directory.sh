91d9428eb50111a54d21b776fb7500e274a84b35
Vinny <vicentmugabo75@gmail.com>
2024-02-18 15:44:20 -0800

    Completed move to directory

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Create directory
+mkdir -p negpod_07-q1
+
+# Move the files 
+cp main.sh select-emails.sh students-list_1023.txt student-emails.txt negpod_07-q1/
+
+echo "Files moved to directory negpod_07-q1."
 
