cc0a6a0fdf2ddf9e083ff758a6f3f464fe077c9d
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-25 10:18:07 -0800

    Updated select emails so it doesn't re-add emails already in list

--- select-emails.sh
+++ select-emails.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
-awk -F '.com' '{print $1".com"}' students-list_1023.txt > student-emails.txt
+awk -F ',' '{print $3}' students-list_1023.txt | while read -r email; do
+    if ! grep -q "^$email$" student-emails.txt; then
+        echo "$email" >> student-emails.txt
+    fi
+done
+
 
 echo "Emails extracted and saved to student-emails.txt"
