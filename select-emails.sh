8c27b715dda7bb3c126a3fd8d3049baa5b7025e8
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-25 10:33:13 -0800

    Changed echo message

--- select-emails.sh
+++ select-emails.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
+count=0
 awk -F ',' '{print $3}' students-list_1023.txt | while read -r email; do
     if ! grep -q "^$email$" student-emails.txt; then
         echo "$email" >> student-emails.txt
-        echo "$email extracted and saved to student-emails.txt"
-    else
-        echo "$email already in student-emails.txt"
+        ((count++))
     fi
 done
+
+echo "$count number of emails extracted and added to student-emails.txt"
 
