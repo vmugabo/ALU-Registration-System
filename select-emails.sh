e832b9e53cd05987d035bd478699a06777bdad98
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-25 10:25:03 -0800

    Added message if email is already in list

--- select-emails.sh
+++ select-emails.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
+
 awk -F ',' '{print $3}' students-list_1023.txt | while read -r email; do
     if ! grep -q "^$email$" student-emails.txt; then
         echo "$email" >> student-emails.txt
+        echo "$email extracted and saved to student-emails.txt"
+    else
+        echo "$email already in student-emails.txt"
     fi
 done
 
-
-echo "Emails extracted and saved to student-emails.txt"
